4c04894c4ee155c5c874057674caea14f4ec3951
Fightdoc2809 <Wurst2809>
2021-05-11 17:59:25 +0200

    Fertig 4

--- main.c
+++ main.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <ctype.h>
 
 
 int main(int argc, char *argv[]) {
+
+    //declaration
     FILE *in = stdin;
-    int num, ausgabe;
+    int ausgabe;
     char buffer[400];
     char delimit[] = ",:;\n";
     char *token;
 
+    //Abfrage bei mehr als 2 Argumenten
     if (argc > 2) {
         fprintf(stderr, "Bitte Maximal 2 Argumente angeben");
         exit(EXIT_FAILURE);
     }
 
+    //Abfrage bei 2 Argumenten
     if (argc == 2) {
-
+        //Datei wird geöffnet und in "in" gespeichert
         in = fopen(argv[1], "r");
         if (!in) {
             perror(argv[0]);
             exit(EXIT_FAILURE);
         }
     }
+    //Eingabe wird aufgesplitet
     while (fgets(buffer, 400, in)) {
         token = strtok(buffer, delimit);
         if (!token) {
@@ -33,6 +36,8 @@ int main(int argc, char *argv[]) {
             }
             exit(EXIT_SUCCESS);
         }
+
+        //Eingabe wird verdoppelt und ausgegeben
         ausgabe = atoi(buffer);
         ausgabe = ausgabe * 2;
         printf("%d\n", ausgabe);
